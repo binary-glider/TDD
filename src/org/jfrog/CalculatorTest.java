fef121f8f5a633605c5f99599ec1e2e302bd0fb7
aaronsrhodes <aaronr@jfrog.com>
2016-06-30 16:24:13 -0700

    Initial commit

---
+++ src/org/jfrog/CalculatorTest.java
@@ -0,0 +1,16 @@
+package org.jfrog;
+
+import junit.framework.*;
+
+
+/**
+ * Created by aaronrhodes on 6/30/16.
+ */
+public class CalculatorTest extends TestCase {
+    private Calculator c = new Calculator();
+
+    public void testAdd(){
+        int i = c.calculate(2, "+", 2);
+        assertEquals(i, 4);
+    }
+}
