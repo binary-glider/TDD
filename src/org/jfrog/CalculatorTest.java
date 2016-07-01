fdcca4924700253989f069f63b38fd66b2571345
aaronsrhodes <aaronr@jfrog.com>
2016-07-01 10:14:30 -0700

    swapped paramiters.

--- src/org/jfrog/CalculatorTest.java
+++ src/org/jfrog/CalculatorTest.java
@@ -10,38 +10,38 @@ public class CalculatorTest extends TestCase {
     private Calculator c = new Calculator();
 
     public void testAdd() {
-        int i = c.calculate(2, "+", 2);
-        assertEquals(i, 4);
+        int i = c.calculate(2, '+', 2);
+        assertEquals(4, i);
     }
 
     public void testSubtract() {
-        int i = c.calculate(2, "-", 2);
-        assertEquals(i, 0);
+        int i = c.calculate(2, '-', 2);
+        assertEquals(0, i);
     }
 
     public void testMultiply() {
-        int i = c.calculate(3, "*", 2);
-        assertEquals(i, 6);
+        int i = c.calculate(3, '*', 2);
+        assertEquals(6, i);
     }
 
     public void testMultiply2() {
-        int i = c.calculate(3, "x", 2);
-        assertEquals(i, 6);
+        int i = c.calculate(3, 'x', 2);
+        assertEquals(6, i);
     }
 
     public void testDivide() {
-        int i = c.calculate(4, "/", 2);
-        assertEquals(i, 2);
+        int i = c.calculate(4, '/', 2);
+        assertEquals(2, i);
     }
 
     public void testDivide2() {
-        int i = c.calculate(4, "÷", 1);
-        assertEquals(i, 4);
+        int i = c.calculate(4, '÷', 1);
+        assertEquals(4, i);
     }
 
     public void testDivideByZero() throws Exception {
         try {
-            c.calculate(4, "/", 0);
+            c.calculate(4, '/', 0);
             fail("division by zero should throw an exception!");
         } catch (RuntimeException expected) {
             // this is exactly what you expect so
@@ -50,35 +50,35 @@ public class CalculatorTest extends TestCase {
     }
 
     public void testIntStrings() {
-        int i = c.calculate("4", "+", "2");
-        assertEquals(i, 6);
+        int i = c.calculate("42", '+', "8");
+        assertEquals(50, i);
     }
 
     public void testNegatives() {
-        int i = c.calculate(2, "+", -4);
-        assertEquals(i, -2);
+        int i = c.calculate(2, '+', -4);
+        assertEquals(-2, i);
 
-        int j = c.calculate(-2, "-", -4);
-        assertEquals(i, 2);
+        int j = c.calculate(-2, '-', -4);
+        assertEquals(2, i);
 
-        int k = c.calculate(-2, "*", 4);
-        assertEquals(i, -8);
+        int k = c.calculate(-2, '*', 4);
+        assertEquals(-8, i);
 
-        int l = c.calculate(-12, "/", 4);
-        assertEquals(i, -3);
+        int l = c.calculate(-12, '/', 4);
+        assertEquals(-3, i);
     }
 
     public void testDoubles() {
-        double i = c.calculate(2.5, "+", 3.14);
+        double i = c.calculate(2.5, '+', 3.14);
         assertEquals(i, 5.64);
 
-        double j = c.calculate(2.5, "-", 3.14);
+        double j = c.calculate(2.5, '-', 3.14);
         assertEquals(i, -0.64);
 
-        double k = c.calculate(2.5, "*", 3.14);
+        double k = c.calculate(2.5, '*', 3.14);
         assertEquals(i, 7.85);
 
-        double l = c.calculate(5.5, "/", 4);
+        double l = c.calculate(5.5, '/', 4);
         assertEquals(i, 1.375);
     }
 
