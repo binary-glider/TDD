92d815c269ab7c4d24cbe30d9fd55440ec1fda82
aaronsrhodes <aaronr@jfrog.com>
2016-07-01 10:08:32 -0700

    swapped paramiters.

--- src/org/jfrog/CalculatorTest.java
+++ src/org/jfrog/CalculatorTest.java
@@ -11,21 +11,21 @@ public class CalculatorTest extends TestCase {
 
     public void testAdd(){
         int i = c.calculate(2, '+', 2);
-        assertEquals(i, 4);
+        assertEquals(4, i);
     }
 
     public void testSubtract(){
         int i = c.calculate(2, '-', 2);
-        assertEquals(i, 0);
+        assertEquals(0, i);
     }
 
     public void testMultiply(){
         int i = c.calculate(3, '*', 2);
-        assertEquals(i, 6);
+        assertEquals(6, i);
     }
 
     public void testDivide(){
         int i = c.calculate(4, '/', 2);
-        assertEquals(i, 2);
+        assertEquals(2, i);
     }
 }
