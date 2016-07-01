8350bfc792b6da3ffb89fc77b02e36d90d884336
aaronsrhodes <aaronr@jfrog.com>
2016-07-01 10:07:49 -0700

    swapped paramiters.

--- src/org/jfrog/CalculatorTest.java
+++ src/org/jfrog/CalculatorTest.java
@@ -9,50 +9,49 @@ import junit.framework.*;
 public class CalculatorTest extends TestCase {
     private Calculator c = new Calculator();
 
-    public void testAdd(){
-        int i = c.calculate(2, "+", 2);
-        assertEquals(i, 4);
+    public void testAdd() {
+        int i = c.calculate(2, '+', 2);
+        assertEquals(4, i);
     }
 
-    public void testSubtract(){
-        int i = c.calculate(2, "-", 2);
-        assertEquals(i, 0);
+    public void testSubtract() {
+        int i = c.calculate(2, '-', 2);
+        assertEquals(0, i);
     }
 
-    public void testMultiply(){
-        int i = c.calculate(3, "*", 2);
-        assertEquals(i, 6);
+    public void testMultiply() {
+        int i = c.calculate(3, '*', 2);
+        assertEquals(6, i);
     }
 
-    public void testMultiply2(){
-        int i = c.calculate(3, "x", 2);
-        assertEquals(i, 6);
+    public void testMultiply2() {
+        int i = c.calculate(3, 'x', 2);
+        assertEquals(6, i);
     }
 
-    public void testDivide(){
-        int i = c.calculate(4, "/", 2);
-        assertEquals(i, 2);
+    public void testDivide() {
+        int i = c.calculate(4, '/', 2);
+        assertEquals(2, i);
     }
 
-    public void testDivide2(){
-        int i = c.calculate(4, "÷", 1);
-        assertEquals(i, 4);
+    public void testDivide2() {
+        int i = c.calculate(4, '÷', 1);
+        assertEquals(4, i);
     }
 
-    public void testDivideByZero(){
-        throws Exception {
-            try {
-                c.calculate(4, "/", 0);;
-                fail("division by zero should throw an exception!");
-            } catch (RuntimeException expected) {
-                // this is exactly what you expect so
-                // just ignore it and let the test pass
-            }
+    public void testDivideByZero() throws Exception {
+        try {
+            c.calculate(4, '/', 0);
+            fail("division by zero should throw an exception!");
+        } catch (RuntimeException expected) {
+            // this is exactly what you expect so
+            // just ignore it and let the test pass
         }
     }
 
-    public void testIntStrings(){
-        int i = c.calculate("4", "+", "2");
-        assertEquals(i, 6);
+
+    public void testIntStrings() {
+        int i = c.calculate("4", '+', "2");
+        assertEquals(6, i);
     }
 }
