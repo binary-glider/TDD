feb541373737b86ad41def443f668e603c79a99d
aaronsrhodes <aaronr@jfrog.com>
2016-07-01 09:55:43 -0700

    changed string to char for operator

--- src/org/jfrog/CalculatorTest.java
+++ src/org/jfrog/CalculatorTest.java
@@ -10,7 +10,7 @@ public class CalculatorTest extends TestCase {
     private Calculator c = new Calculator();
 
     public void testAdd(){
-        int i = c.calculate(2, "+", 2);
+        int i = c.calculate(2, '+', 2);
         assertEquals(i, 4);
     }
 }
