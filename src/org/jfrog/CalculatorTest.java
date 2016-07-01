eac20ec1a6b07b0bc9c7f8ad6a59886957d61631
aaronsrhodes <aaronr@jfrog.com>
2016-06-30 17:46:18 -0700

    Alternate symbols Ints as strings Divide by zero

--- src/org/jfrog/CalculatorTest.java
+++ src/org/jfrog/CalculatorTest.java
@@ -24,8 +24,35 @@ public class CalculatorTest extends TestCase {
         assertEquals(i, 6);
     }
 
+    public void testMultiply2(){
+        int i = c.calculate(3, "x", 2);
+        assertEquals(i, 6);
+    }
+
     public void testDivide(){
         int i = c.calculate(4, "/", 2);
         assertEquals(i, 2);
+    }
+
+    public void testDivide2(){
+        int i = c.calculate(4, "÷", 1);
+        assertEquals(i, 4);
+    }
+
+    public void testDivideByZero(){
+        throws Exception {
+            try {
+                c.calculate(4, "/", 0);;
+                fail("division by zero should throw an exception!");
+            } catch (RuntimeException expected) {
+                // this is exactly what you expect so
+                // just ignore it and let the test pass
+            }
+        }
+    }
+
+    public void testIntStrings(){
+        int i = c.calculate("4", "+", "2");
+        assertEquals(i, 6);
     }
 }
