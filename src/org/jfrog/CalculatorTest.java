f5438bf8ad2c74f95c62c668bf68ce827ee6a217
aaronsrhodes <aaronr@jfrog.com>
2016-06-30 17:36:50 -0700

    Multiply and Divide

--- src/org/jfrog/CalculatorTest.java
+++ src/org/jfrog/CalculatorTest.java
@@ -18,4 +18,14 @@ public class CalculatorTest extends TestCase {
         int i = c.calculate(2, "-", 2);
         assertEquals(i, 0);
     }
+
+    public void testMultiply(){
+        int i = c.calculate(3, "*", 2);
+        assertEquals(i, 6);
+    }
+
+    public void testDivide(){
+        int i = c.calculate(4, "/", 2);
+        assertEquals(i, 2);
+    }
 }
